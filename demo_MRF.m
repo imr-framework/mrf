21c830c1d72b79d3fec591cca4fc23611ee405cc
Enlin Qian <eq2144@columbia.edu>
2019-08-15 12:31:27 -0400

    Add sample raw data

--- demo_MRF.m
+++ demo_MRF.m
@@ -10,7 +10,11 @@ addpath(genpath('.'));
 image_data_final_Complex = MRF_recon();
 
 %% Dictionary simulation
-
+phis = zeros(1,1001); % the sequence starts with a 180 pulse, thus 1001 points
+alphas = [180, FA_all']; % all flip angles
+TRs = [18, TR_all'.*1000]; % all TRs, in ms
+T1_range = [];
+T2_range = [];
 %% Dictionary matching
 
 %% ROI analysis tool
