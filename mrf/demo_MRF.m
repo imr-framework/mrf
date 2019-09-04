c942e7b3fa2fceedbed5690c00569587beca7fd5
Enlin Qian <eq2144@columbia.edu>
2019-09-04 16:12:32 -0400

    Add dictionary simulation to demo_MRF.m

--- mrf/demo_MRF.m
+++ mrf/demo_MRF.m
@@ -10,21 +10,18 @@ addpath(genpath('.'));
 image_data_final_Complex = MRF_recon();
 
 %% Dictionary Simulation
-phis = zeros(1,1001); % the sequence starts with a 180 pulse, thus 1001 points
-alphas = [180, FA_all']; % all flip angles
-TRs = [18, TR_all'.*1000]; % all TRs, in ms
-T1_range=20:20:2000; 
-T2_range=5:20:600; 
-n3 = 1;
-Echo_Final=zeros(length(T1_range)*length(T2_range), 1000);
-for n1 = 1:length(T1_range)
-    for n2 = 1:length(T2_range)
-        disp (n3)
-        [om_store,echoes,seq] = EPGsim_MRF(phis,alphas,TRs,[T1_range(n1), T2_range(n2)]);
-        Echo_Final(n3, :) = Var_TE(om_store, TE_all, T2_range(n2));
-        n3=n3+1;
-    end
-end
+T1=0.02:0.005:0.065;
+T1=[T1, 0.09:0.03:0.36];
+T1=[T1, 0.5:0.1:4];
+T2=0.005:0.002:0.011;
+T2=[T2, 0.015:0.005:0.045];
+T2=[T2, 0.065:0.03:0.245];
+T2=[T2, 0.35:0.05:0.5];
+T2=[T2, 0.7:0.1:2];
+TI=18;
+kshots=48;
+MRF_dict = gen_MRF_dict(TR_all, TE_all, FA_all, T1, T2, TI, kshots);
+
 %% Dictionary Matching
 
 %% ROI Analysis Tool
